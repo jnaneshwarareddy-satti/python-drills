df03afd9a4162945cc0f527c884a7d99d14a8296
jnaneshwarareddy-satti <jnaneshwarareddysatti@gmail.com>
2026-07-24 10:33:29 +0530

    filesystem.py is updated

--- filesystem.py
+++ filesystem.py
@@ -1,27 +1,14 @@
 from pathlib import Path
 import sys
 
-try:
-    pt = sys.argv[1]
+pt = sys.argv[1]
+folder = Path(pt)
+notebook = dict()
 
-except IndexError:
-    print("please enter path ")
+for item in folder.iterdir():
+    if item.is_file():
+        notebook[item] = format(item.stat().st_size / 1024, ".2f")
 
-else:
-    folder = Path(pt)
-    notebook = {}
-    # fill notebook with name and its size
+sorted_notebook = dict(sorted(notebook.items(), key=lambda item: item[1]))
 
-    try:
-        for item in folder.iterdir():
-            if item.is_file():
-                notebook[item.name] = item.stat().st_size
-    except FileNotFoundError:
-        print("path not found..")
-    else:
-
-        # sorting notebook
-        sorted_notebook = dict(sorted(notebook.items(), key=lambda item: item[1]))
-        # printing sorted notebook , 1kb=1024 bytes , xbytes=x/1024 kb
-        for file in sorted_notebook.items():
-            print(f"{file[0]} = {(file[1]/1024):.2f}kb", end=" | ")
+print(sorted_notebook)
